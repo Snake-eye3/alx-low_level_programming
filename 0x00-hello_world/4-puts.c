a1357ab925321e3b16a19bf97758395b85a2e921
Snake-eye3 <anasabdulhameed718@gmail.com>
2022-12-10 10:49:07 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
-/** 
- * main - Entry point
- *
- * Return: Always 0 (Succcess)
- */
 int main(void) 
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
