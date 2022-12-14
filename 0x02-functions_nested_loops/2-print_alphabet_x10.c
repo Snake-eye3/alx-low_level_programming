7117d67a8ad442a917c79449a6d9e809fa5779c7
Snake-eye3 <anasabdulhameed718@gmail.com>
2022-12-14 10:13:34 -0800

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ *print_alphabet_x10 - this function will print the alphabet 10 times.
+ */
+void print_alphabet_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; line <= 9; ++line)
+	{
+	for (ch = 'a'; ch <= 'z'; ++ch)
+	_putchar(ch);
+	_putchar('\n');
+	}
+}
