39fc788cc498253c1fc508946a0ed6ad66a4430a
Snake-eye3 <anasabdulhameed718@gmail.com>
2022-12-14 10:46:38 -0800

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *  _isalpha - code entry.
+ *
+ *  Description: A function that checks for alphabetic characters.
+ *
+ *  @c: The intrger value it receives.
+ *
+ *  Return: 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+	char cap_alphabet, icap_alphabet;
+
+	for (cap_alphabet = 'a'; cap_alphabet <= 'z'; cap_alphabet++)
+	{
+		for (icap_alphabet = 'A'; icap_alphabet <= 'Z'; icap_alphabet++)
+		{
+			if ((cap_alphabet == c) || (icap_alphabet == C))
+			{
+				return(1);
+			}
+		}
+	}
+	return(0);
+}
